7cbfe5c1533f59e7cb81ef28542c02909f53c985
Yogesh <coolcoder9334@gmail.com>
2023-12-16 23:58:33 +0530

    Added the movie description

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -29,10 +29,14 @@ const Main = () => {
             <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
              <img className='w-full h-full object-cover' src={ `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}></img>
              <div className='absolute w-full top-[20%] p-4 md:p-8'>
+                <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+                <div className='my-4'></div>
                 <div>
                     <button className='border bg-gray-300 text-black py-2 px-5'>Play</button>
                     <button className='border border-gray-300 text-white py-2 px-5 ml-4'>Watch Later</button>
                 </div>
+                <div className=' text-gray-400 py-2 text-sm'>Released:{movie.release_date}</div>
+                <div className='w-full md:max-w-[70%] lg:mx-w-[50%] xl:mx-w-[35%] text-gray-200'>{movie.overview}</div>
              </div>
         </div>
     </div>
